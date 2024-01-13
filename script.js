3a4c873bf4c310a7100750701c6ea2e196fe6c40
Abesh <abeshsarkar2030@gmail.com>
2024-01-13 23:33:53 +0530

    final project

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let button = document.getElementById("btn");
-let image = document.getElementById("img");
-let text = document.querySelector("#para");
+let slider = document.querySelector(".toggle");
+let circle = document.querySelector("#circle-logo");
+let text = document.querySelector(".para");
+let sun = document.querySelector("#on");
+let moon = document.querySelector("#off");
 
+// let isDarkMode = true;
+// let lightMode = true;
+document.body.style.backgroundColor = "white";
 
-
-image.addEventListener("click", ()=>{
-    // image.style.transform = "transition(0.2s)";
-    if(image.style.transform === "translateX(-60px)"){
-        image.style.transform = "translateX(20px)"
-    } else {
-        image.style.transform = "translateX(-60px)"
-    }
-})
-let flag=true;
-
-button.addEventListener("click", ()=>{
-    document.body.classList.toggle("bg-color");
-    if(flag){
-    text.setAttribute("class","para1");
-flag=false;}
-else{
-text.setAttribute("class","para");
-flag=true  
-console.log(text);
-}})
-
-
+slider.addEventListener("click", () => {
+  if (document.body.style.backgroundColor === "white") {
+    document.body.style.backgroundColor = "black";
+    text.style.color = "white";
+    circle.style.left = "55%";
+    // circle.style.backgroundColor = "Orange";
+    text.style.color = "blue";
+    // x.style.transform = rotate(${isDarkMode ? 360 : 0}deg);
+    // isDarkMode = !isDarkMode;
+    // x.style.transitionDuration = "0.5s";
+    // text.style.transform = "translateX(-20px)";
+    text.style.transform = "translateY(40px)";
+    text.style.transitionDuration = "0.4s"
+  } else {
+    document.body.style.backgroundColor = "white";
+    text.style.color = "black";
+    circle.style.left = "2%";
+    circle.style.backgroundColor = "white";
+    text.style.color = "black";
+    // y.style.transform = rotate(${lightMode ? 360 : 0}deg);
+    // lightMode = !lightMode;
+    // y.style.transitionDuration = ".5s";
+    text.style.transform = "translateY(60px)";
+    text.style.transitionDuration = "0.4s";
+  }
+});
